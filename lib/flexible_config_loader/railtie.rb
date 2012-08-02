bffc67ef2d9e64b2a286450d437ddde58495ddf3
Nathan Wenneker <nathan@gladtocode.com>
2012-08-02 10:18:50 -0600

    modifications

--- lib/flexible_config_loader/railtie.rb
+++
@@ -1,8 +0,0 @@
-module FlexibleConfigLoader
-  class Railtie < Rails::Railtie
-    before_initialize 'flexible_config_loader.load_config' do
-      FlexibleConfigLeader.load_config
-    end
-  end
-end
-
